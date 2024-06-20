851142857c9149d3de0a3c8e797e559881439922
SarahOtmane <sarahotmane02@gmail.com>
2024-06-20 14:52:36 +0200

    listArtist composant

---
+++ lib/screen/listArtist.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ListArtistScreen extends StatelessWidget {
+  const ListArtistScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            
+          ),
+        ],
+      ),
+    );
+  }
+}
