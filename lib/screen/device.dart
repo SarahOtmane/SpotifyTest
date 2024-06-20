1248228533eb35d44a44cc0f54d39c4b804197bb
SarahOtmane <sarahotmane02@gmail.com>
2024-06-20 11:38:50 +0200

    composant device.Dart

---
+++ lib/screen/device.dart
@@ -0,0 +1,38 @@
+// screen/device.dart
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class DeviceScreen extends StatefulWidget {
+  @override
+  _DeviceScreenState createState() => _DeviceScreenState();
+}
+
+class _DeviceScreenState extends State<DeviceScreen> {
+  int _currentScreen = 0;
+  final List<Widget> _screenList = [
+    const HomeScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+      body: _screenList[_currentScreen],
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.red,
+          unselectedItemColor: Colors.black,
+          iconSize: 35,
+          currentIndex: _currentScreen,
+          onTap: onTabTapped,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          ]),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentScreen = index;
+    });
+  }
+}
