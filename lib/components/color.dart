873ce94bb81d84a8456b3003741005ccb7ed937c
SarahOtmane <sarahotmane02@gmail.com>
2024-06-21 09:38:49 +0200

    fichier color

---
+++ lib/components/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightgrey = Color(0xFFD9D9D9);
+  static const Color primary = Color(0xFFB503D2);
+  static const Color grey = Color(0xFF606060);
+  static const Color darkGrey = Color(0xFF373737);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+}
