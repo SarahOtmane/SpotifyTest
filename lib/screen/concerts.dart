db54a6aef22d78f0d1321e0e534e26971ea5d2d3
SarahOtmane <sarahotmane02@gmail.com>
2024-06-21 12:13:10 +0200

    composant ConcertScreen

--- lib/screen/concerts.dart
+++ lib/screen/concerts.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import '../components/artistConcertCard.dart';
+import '../data/concertArtistData.dart';
 
-class ConcertsScreen extends StatelessWidget{
-  const ConcertsScreen({super.key});
+class ConcertsScreen extends StatelessWidget {
+  ConcertsScreen({super.key});
+
+  final List<Concert> _displayedConcerts = concerts;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
       body: SingleChildScrollView(
-        child: Text('Recherche concerts'),
+        child: ListView.builder(
+          shrinkWrap: true, // Ajouté pour rendre la ListView flexible dans le ScrollView
+          physics: NeverScrollableScrollPhysics(), // Pour désactiver le défilement de la ListView
+          itemCount: _displayedConcerts.length,
+          itemBuilder: (context, index) {
+            final concert = _displayedConcerts[index];
+            return artistConcertCard(
+              concertName: concert.name,  // Ajout de l'URL de l'image
+              genres: concert.genre, // Correctement référencé depuis concert
+              onTap: () {
+                // Gérer la sélection de l'artiste
+              },
+            );
+          },
+        ),
       ),
     );
   }
