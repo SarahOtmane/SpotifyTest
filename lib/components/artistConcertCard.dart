ea4df4297ce9e6b0311fe495f713bbbeb425a9e3
SarahOtmane <sarahotmane02@gmail.com>
2024-06-21 14:22:55 +0200

    listing des ArtistConcertCard

--- lib/components/artistConcertCard.dart
+++ lib/components/artistConcertCard.dart
@@ -1,29 +1,100 @@
 import 'package:flutter/material.dart';
 import '../components/color.dart';
 
-class artistConcertCard extends StatelessWidget {
+class ArtistConcertCard extends StatelessWidget {
+  final String date;
   final String concertName;
+  final String lieux;
+  final String lien;
   final List<String> genres;
   final VoidCallback onTap;
 
-  artistConcertCard({
+  ArtistConcertCard({
     required this.concertName,
     required this.genres,
+    required this.date,
+    required this.lieux,
+    required this.lien,
     required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(16), // Ajout de la marge ici
+      margin: EdgeInsets.all(16),
       decoration: BoxDecoration(
         color: AppColors.grey,
         borderRadius: BorderRadius.circular(10),
       ),
-      padding: EdgeInsets.all(8),
-      child: Row(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-        ]
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                concertName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                date,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 8),
+          Text(
+            lieux,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(height: 16),
+          Row(
+            children: genres.map((genre) => Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: Chip(
+                label: Text(genre),
+                backgroundColor: AppColors.primarylight,
+                labelStyle: TextStyle(color: Colors.white),
+              ),
+            )).toList(),
+          ),
+          SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              GestureDetector(
+                onTap: onTap,
+                child: Row(
+                  children: [
+                    Text(
+                      'Acheter mes billets',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Icon(
+                      Icons.open_in_new,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
