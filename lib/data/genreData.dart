0796ba77831e4fd74d64e64ec942c694f3d0d855
SarahOtmane <sarahotmane02@gmail.com>
2024-06-26 15:33:25 +0200

    update genreData

--- lib/data/genreData.dart
+++ lib/data/genreData.dart
@@ -1,40 +1,32 @@
 class Genre {
-  final String name;
-  final String id;
+  String name;
+  bool isSelected;
 
-  Genre({
-    required this.name,
-    required this.id,
-  });
+  Genre({required this.name, this.isSelected = false});
 }
 
 List<Genre> genres = [
-  Genre(
-    name: 'pop',
-    id: '1',
-  ),
-  Genre(
-    name: 'rock',
-    id: '2',
-  ),
-  Genre(
-    name: 'jazz',
-    id: '3',
-  ),
-  Genre(
-    name: 'pop',
-    id: '4',
-  ),
-  Genre(
-    name: 'techno',
-    id: '5',
-  ),
-  Genre(
-    name: 'rock',
-    id: '5',
-  ),
-  Genre(
-    name: 'rock & roll',
-    id: '5',
-  ),
+  Genre(name: 'Action'),
+  Genre(name: 'Rock'),
+  Genre(name: 'Pop'),
+  Genre(name: 'Folk'),
+  Genre(name: 'Danse/Electric'),
+  Genre(name: 'Disco'),
+  Genre(name: 'Indie Rock'),
+  Genre(name: 'World'),
+  Genre(name: 'Classic'),
+  Genre(name: 'Jazz'), 
+  Genre(name: 'Blues'),
+  Genre(name: 'Country'),
+  Genre(name: 'Hip Hop'),
+  Genre(name: 'Rap'),
+  Genre(name: 'R&B'),
+  Genre(name: 'Soul'),
+  Genre(name: 'Reggae'),
+  Genre(name: 'Metal'),
+  Genre(name: 'Punk'),
+  Genre(name: 'Funk'),
+  Genre(name: 'Techno'),
+  Genre(name: 'House'),
+  Genre(name: 'Trance'),
 ];
