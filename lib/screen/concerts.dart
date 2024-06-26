7cdd300256a606f887645d18bce30da728b403b7
SarahOtmane <sarahotmane02@gmail.com>
2024-06-26 10:14:34 +0200

    update concerts

--- lib/screen/concerts.dart
+++ lib/screen/concerts.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../components/artistConcertCard.dart';
 import '../components/color.dart';
-
 import '../components/searchBar.dart';
 import '../data/concertArtistData.dart';
 
@@ -10,23 +9,43 @@ class ConcertsScreen extends StatefulWidget {
   _ConcertScreenState createState() => _ConcertScreenState();
 }
 
-class _ConcertScreenState extends State<ConcertsScreen>{
+class _ConcertScreenState extends State<ConcertsScreen> {
   TextEditingController _searchController = TextEditingController();
-  List<Concert> _displayedConcerts = concerts;
+  List<Concert> _displayedConcerts = [];
+  bool _isDefaultCity = true;
 
+  @override
+  void initState() {
+    super.initState();
+    _displayConcertsForDefaultCity();
+  }
 
-  void _filterConcerts(String query) {
+  void _displayConcertsForDefaultCity() {
     final filteredConcerts = concerts.where((concert) {
-      final concertLower = concert.lieux.toLowerCase();
-      final queryLower = query.toLowerCase();
-
-      return concertLower.contains(queryLower);
+      return concert.lieux.toLowerCase().contains('paris');
     }).toList();
     setState(() {
       _displayedConcerts = filteredConcerts;
+      _isDefaultCity = true;
     });
   }
 
+  void _filterConcerts(String query) {
+    if (query.isNotEmpty) {
+      final filteredConcerts = concerts.where((concert) {
+        final concertLower = concert.lieux.toLowerCase();
+        final queryLower = query.toLowerCase();
+
+        return concertLower.contains(queryLower);
+      }).toList();
+      setState(() {
+        _displayedConcerts = filteredConcerts;
+        _isDefaultCity = false;  
+      });
+    } else {
+      _displayConcertsForDefaultCity();  
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,21 +58,31 @@ class _ConcertScreenState extends State<ConcertsScreen>{
             onChanged: _filterConcerts,
             placeholder: 'Chercher une ville',
           ),
-
+          if (_isDefaultCity) 
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Ville par défaut : Paris',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
           Expanded(
             child: ListView.builder(
               itemCount: _displayedConcerts.length,
               itemBuilder: (context, index) {
                 final concert = _displayedConcerts[index];
                 return ArtistConcertCard(
-                concertName: concert.name,  
-                genres: concert.genre, 
-                date: concert.date,
-                lieux: concert.lieux,
-                lien: concert.lien,
-                onTap: () {
-                  
-                },
+                  concertName: concert.name,
+                  genres: concert.genre,
+                  date: concert.date,
+                  lieux: concert.lieux,
+                  lien: concert.lien,
+                  onTap: () {
+                    // Handle artist selection
+                  },
                 );
               },
             ),
