a8948512c5b4cfe2c37c9b28d0db389a4fd53a03
SarahOtmane <sarahotmane02@gmail.com>
2024-06-26 09:53:22 +0200

    add searchBar

--- lib/screen/concerts.dart
+++ lib/screen/concerts.dart
@@ -1,21 +1,63 @@
 import 'package:flutter/material.dart';
-import '../data/concertArtistData.dart';
 import '../components/artistConcertCard.dart';
 import '../components/color.dart';
 
-class ConcertsScreen extends StatelessWidget {
-  ConcertsScreen({super.key});
+import '../components/searchBar.dart';
+import '../data/concertArtistData.dart';
 
-  final List<Concert> _displayedConcerts = concerts;
+class ConcertsScreen extends StatefulWidget {
+  @override
+  _ConcertScreenState createState() => _ConcertScreenState();
+}
+
+class _ConcertScreenState extends State<ConcertsScreen>{
+  TextEditingController _searchController = TextEditingController();
+  List<Concert> _displayedConcerts = concerts;
+
+
+  void _filterConcerts(String query) {
+    final filteredConcerts = concerts.where((concert) {
+      final concertLower = concert.lieux.toLowerCase();
+      final queryLower = query.toLowerCase();
+
+      return concertLower.contains(queryLower);
+    }).toList();
+    setState(() {
+      _displayedConcerts = filteredConcerts;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.black,
       body: Column(
-        children : [
-          
-        ]
+        children: [
+          SearchInput(
+            controller: _searchController,
+            onChanged: _filterConcerts,
+          ),
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: _displayedConcerts.length,
+              itemBuilder: (context, index) {
+                final concert = _displayedConcerts[index];
+                return ArtistConcertCard(
+                concertName: concert.name,  
+                genres: concert.genre, 
+                date: concert.date,
+                lieux: concert.lieux,
+                lien: concert.lien,
+                onTap: () {
+                  
+                },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
